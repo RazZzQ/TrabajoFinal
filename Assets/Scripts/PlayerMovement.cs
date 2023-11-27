dfc7948072b05570185c5cdf729aa443023b297e
RazZzQ <roswell.quispe@tecsup.edu.pe>
2023-11-27 14:10:28 -0500

    Nada Importante

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,8 +4,6 @@ using UnityEngine.InputSystem;
 public class PlayerMovement : MonoBehaviour
 {
     public float speed = 5f;
-    public float lookSpeed = 2f; // Ajusta la velocidad de rotación del jugador al girar la cámara.
-
     private Rigidbody playerRigidbody;
     private Vector3 movement;
 
@@ -29,22 +27,6 @@ public class PlayerMovement : MonoBehaviour
     public void OnLook(InputAction.CallbackContext context)
     {
         Vector2 mouseDelta = context.ReadValue<Vector2>();
-
-        // Obtén la rotación actual del jugador.
-        Vector3 currentRotation = transform.eulerAngles;
-
-        // Calcula la nueva rotación del jugador en función del input del ratón.
-        currentRotation.y += mouseDelta.x * lookSpeed;
-
-        // Aplica la nueva rotación al jugador.
-        transform.eulerAngles = currentRotation;
-
-        // Accede al componente CinemachinePOV para ajustar la rotación de la cámara.
-        CinemachinePOV pov = GetComponentInChildren<CinemachinePOV>();
-        if (pov != null)
-        {
-            // Ajusta la rotación de la cámara basándote en el input del ratón.
-            pov.m_Rig.Rotate(Vector3.up, mouseDelta.x * lookSpeed);
-        }
+        Debug.Log(mouseDelta);
     }
 }
