4a421083035b68a19ea8c3ee04e99fa3094e8c4d
RazZzQ <roswell.quispe@tecsup.edu.pe>
2023-11-30 05:30:52 -0500

    Al fin logre solucionar la camara trostemente no con el new input pipipi

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -8,8 +8,14 @@ public class PlayerMovement : MonoBehaviour
     public float verticalSpeed = 10f;
     public float clampAngle = 80f;
     public float speed = 5f;
-    CameraState state;
+
+    public PlayerMovement player;
+    public CinemachineVirtualCamera Camera;
+    private Vector3 camForward;
+    private Vector3 camRight; 
+
     private Rigidbody playerRigidbody;
+
     private Vector3 movement;
     private Vector3 Rotation;
 
@@ -27,17 +33,28 @@ public class PlayerMovement : MonoBehaviour
     {
         Vector2 movementInput = context.ReadValue<Vector2>();
         movement = new Vector3(movementInput.x, 0f, movementInput.y);
+        
         movement.Normalize();
+        camDirection();
+
+        movement = movementInput.x * camRight +movementInput.y * camForward;
+
+        player.transform.LookAt(player.transform.position + movement);
+    }
+    public void camDirection()
+    {
+        camForward = Camera.transform.forward;
+        camRight = Camera.transform.right;
+
+        camForward.y = 0f;
+        camRight.y = 0f;
+
+        camForward = camForward.normalized;
+        camRight = camRight.normalized;
     }
 
     public void OnLook(InputAction.CallbackContext context)
     {
-        Vector2 mouseDelta = context.ReadValue<Vector2>();
-        if(Rotation == null) Rotation = transform.localRotation.eulerAngles;
-        Rotation.x += mouseDelta.x * Time.deltaTime;
-        Rotation.y += mouseDelta.y * Time.deltaTime;
-        Rotation.y = Mathf.Clamp(Rotation.y, -clampAngle, clampAngle);
-        state.RawOrientation = Quaternion.Euler(Rotation.y, Rotation.x, 0f);
-        Debug.Log(mouseDelta);
+
     }
 }
