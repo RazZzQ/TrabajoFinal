0c0532fd6640e5565319f3870398030cf7ded5cf
RazZzQ <roswell.quispe@tecsup.edu.pe>
2023-11-30 03:24:23 -0500

    Ahora la linterna parpadea hay luz en la oscuridad

--- Assets/Scripts/ControlerLinterna.cs
+++ Assets/Scripts/ControlerLinterna.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,11 +7,29 @@ using UnityEngine.InputSystem;
 public class ControlerLinterna : MonoBehaviour
 {
     public Light LinternaPlayer;
+    public float LifeLintern = 100;
+    public float currentLife = 0;
+    public float TiempoParaEncontrarBateriaNueva = 5;
+
+
+    public event Action OnLifeHalfLintern;
+    public event Action OnFlashLigthOut;
+
+    private bool canToggleFlashlight = true;
+
+    private void Start()
+    {
+        currentLife = LifeLintern;
+    }
+    private void Update()
+    {
+        flashlightflashing();
+    }
     public void OnClick(InputAction.CallbackContext context)
     {
         if (context.performed)
         {
-            if (Input.GetMouseButtonDown(0))
+            if (Input.GetMouseButtonDown(0) && canToggleFlashlight)
             {
                 if (LinternaPlayer.enabled == true)
                 {
@@ -22,5 +41,45 @@ public class ControlerLinterna : MonoBehaviour
                 }
             }
         }
+    }
+    public void flashlightflashing()
+    {
+        if (currentLife <= 0)
+        {
+            // Linterna agotada, ejecuta el evento correspondiente y sal del método
+            OnFlashLigthOut?.Invoke();
+            return;
+        }
+
+        // Verifica si la vida de la linterna está a la mitad y ejecuta el evento correspondiente
+        if (currentLife <= 50)
+        {
+            if (LinternaPlayer.enabled && canToggleFlashlight)
+            {
+                StartCoroutine(FlashlightToggle());
+            }
+            OnLifeHalfLintern?.Invoke();
+        }
+    }
+    private IEnumerator FlashlightToggle()
+    {
+        // Desactiva la capacidad de cambiar el estado de la linterna temporalmente
+        canToggleFlashlight = false;
+
+        // Apaga la linterna
+        LinternaPlayer.enabled = false;
+
+        // Espera un tiempo aleatorio para simular el parpadeo
+        float timeToWait = UnityEngine.Random.Range(0.1f, 2f);
+        yield return new WaitForSeconds(timeToWait);
+
+        // Enciende la linterna
+        LinternaPlayer.enabled = true;
+
+        // Espera un tiempo aleatorio antes de permitir cambiar el estado nuevamente
+        yield return new WaitForSeconds(UnityEngine.Random.Range(0.1f, 2f));
+
+        // Habilita la capacidad de cambiar el estado de la linterna
+        canToggleFlashlight = true;
     }
 }
